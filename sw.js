73f24e2de85c812e2b1204f012ee834ce84d721d
foceko135-prog <foceko135@yahoo.co.jp>
2026-07-13 16:36:19 +0900

    update: 2026-07-13 16:36:02

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 最小キャッシュ制御
-const CACHE='pulse-202607131536';
+const CACHE='pulse-202607131636';
 const CORE=['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
