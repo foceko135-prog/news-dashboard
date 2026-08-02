e366860b6d5140ec47c7c9f6dc234d6c4cd9bd57
foceko135-prog <foceko135@yahoo.co.jp>
2026-08-03 04:06:21 +0900

    update: 2026-08-03 04:06:03

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 最小キャッシュ制御
-const BUILD='pulse-202608030351';
+const BUILD='pulse-202608030406';
 const CACHE='pulse-assets';
 const ASSETS=["./app.88a0647902.css", "./app.a8f41d33ce.js"];
 const CORE=['./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
